8157302317ea2d5c1d4bc547520c0bf77506b312
Evshaddock <evshaddock@gmail.com>
2019-12-27 02:49:02 -0600

    First Blood

---
+++ script.js
@@ -0,0 +1,31 @@
+let textBox = document.getElementById("logo");
+
+setTimeout(deleteText, 1000);
+
+function addText() {
+	let newText = "Hi, I'm Robin! ★";
+  let n = 0;
+  function typeNewText() {
+  	if (n <= newText.length) {
+  		textBox.innerHTML = newText.slice(0, n);
+    	n++;
+    	setTimeout(typeNewText, Math.floor((Math.random() * 100) + 40));
+  	}
+  }
+  if (n == 0) {
+  	typeNewText();
+  }
+}
+
+function deleteText() {
+	let oldText = textBox.innerHTML;
+  if (oldText.length > 0) {
+  	textBox.innerHTML = oldText.slice(0, oldText.length - 1);
+    setTimeout(deleteText, Math.floor((Math.random() * 100) + 40));
+  } else if (oldText.length == 0) {
+  	addText();
+  }
+}
+
+// Code heavily adapted from dcookwebdev on FreeCodeCamp
+// original source https://jsfiddle.net/q9tn70uo/1/
